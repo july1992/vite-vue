cb0ad482ef5f37994bb4c6f2d645fb3a5274827b
vily <1149745884@qq.com>
2023-08-15 09:26:03 +0800

    主体结构

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Login from './components/Login.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/main/MainPage.vue'
 
-createApp(App).mount('#app')
+const app=createApp(App)
+
+app.component("Login",Login)
+app.component("HelloWorld",HelloWorld)
+app.component("MainPage",MainPage)
+
+app.mount('#app')
